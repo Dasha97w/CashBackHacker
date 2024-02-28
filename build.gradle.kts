7d178b84d7ee8808af0e28ceb0ff384d041c5941
Neto test <dashokevgenevna@gmail.com>
2024-02-29 01:12:18 +0300

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testng:testng:7.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
 }
